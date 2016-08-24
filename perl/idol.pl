bdb7a4f446aa3a91c5c3deb99d1244bbea47d95e
unstabler <doping.cheese@gmail.com>
2016-08-25 02:14:21 +0900

    Perl: 샘플 스크립트 추가

--- perl/idol.pl
+++ perl/idol.pl
@@ -1,1 +1,21 @@
-print "나 아이돌 그만 둘래!"
+#!/usr/bin/env perl
+use 5.008001;
+use utf8;
+use strict;
+use warnings;
+
+use lib 'lib';
+use Acme::Idol;
+
+BEGIN {
+    binmode STDOUT, ":encoding(utf8)";
+    binmode STDERR, ":encoding(utf8)";
+}
+
+my $idol = Acme::Idol->new({
+    name => '미오',
+    retire_message => '손님 엄청 적잖아! 나 아이돌 그만둘래!'
+});
+
+
+printf("안녕하세요! 신인 아이돌 %s입니다!\n", $idol->name);
