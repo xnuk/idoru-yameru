559dad9e6580211eddac246e0f0018219e513062
Jaehoon You <teslamint@users.noreply.github.com>
2016-08-25 23:06:24 +0900

    add C#(use mono)

---
+++ csharp/idol.cs
@@ -0,0 +1,10 @@
+// how to compile and run(use mono): mcs idol.cs; mono idol.exe
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("나 아이돌 그만둘래!");
+    }
+}
