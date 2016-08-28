80213b23dede357688e56f9cbb405e44263c06f7
LaruYan <LaruYan@users.noreply.github.com>
2016-08-28 22:20:43 +0900

    Powershell is picky for executing something.
    PowerShell은 무언가를 실행시키기 까따롭습니다!

---
+++ PowerShell/aidoru.ps1
@@ -0,0 +1,17 @@
+# for PowerShell on Windows
+################################################################################
+# HOW TO RUN
+################################################################################
+# open command prompt via start menu
+# or open dialog box by pressing Windows + R key.
+# then type and enter below. you must replace .\aidoru.ps1 to your script file path:
+# code>
+# powerShell -executionPolicy bypass .\aidoru.ps1
+#
+################################################################################
+# REMARKS
+################################################################################
+# setting execution policy as bypass is vital to run unsigned script on powershell script.
+#
+
+echo '나 아이돌 그만둘래'
