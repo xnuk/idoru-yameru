7030d1ce0834e4d749b7e140842fc2f8f3f7ebc1
LaruYan <LaruYan@users.noreply.github.com>
2016-08-28 22:24:19 +0900

    fix some comments typo

--- PowerShell/aidoru.ps1
+++ PowerShell/aidoru.ps1
@@ -3,9 +3,9 @@
 # HOW TO RUN
 ################################################################################
 # open command prompt via start menu
-# or open dialog box by pressing Windows + R key.
+# or open Run dialog by pressing Windows + R key.
 # then type and enter below. you must replace .\aidoru.ps1 to your script file path:
-# code>
+#
 # powerShell -executionPolicy bypass .\aidoru.ps1
 #
 ################################################################################
