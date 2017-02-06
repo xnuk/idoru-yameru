ddc4678a0ccf63437c2943d7459e664979a352b1
Hoto Cocoa (Lee Sung Jae) <cocoa@hoto.us>
2017-02-06 21:02:17 +0900

    봇 코어 완성

--- discord/index.js
+++ discord/index.js
@@ -1,1 +1,18 @@
+const Eris = require("eris");
+var config = require("config.json");
+var bot = new Eris(config.token);
 
+bot.on("ready", () => {
+	//Discord Ready Message
+	console.log('Bot Ready');
+	//Discord Bot Status Set
+	bot.editStatus('online', {name: '아이돌 그만두기'});
+});
+//Discord Message Created
+bot.on("messageCreate", (msg) => {
+	//Check Command
+	if(msg.cleanContent == '!idol') {
+		//Say
+		bot.createMessage(msg.channel.id, '나 아이돌 그만둘래!');
+}
+bot.connect();
