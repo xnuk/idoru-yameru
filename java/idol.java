2a8b51692cb730a5a23907b0384c3ae058b98b1f
saru <sarguments@gmail.com>
2018-04-29 22:32:12 +0900

    인텔리제이 포맷팅, .gitignore 추가

--- java/idol.java
+++ java/idol.java
@@ -1,5 +1,5 @@
-class IdolYameru{
-	public static void main (String[] args){
-		System.out.println("나 아이돌 그만 둘래!");
-	}
+class IdolYameru {
+    public static void main(String[] args) {
+        System.out.println("나 아이돌 그만 둘래!");
+    }
 }
