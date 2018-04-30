a9a8e6687587cb9fbf73cf167be50bf775fc9042
saru <sarguments@gmail.com>
2018-04-30 10:31:00 +0900

    코드 정리

--- java/tdd/test/mio/model/IdoruTest.java
+++ java/tdd/test/mio/model/IdoruTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class IdoruTest {
     @Test
     public void 아이도루테스트() {
-        mio.model.Idoru idoru = new mio.model.Idoru();
+        Idoru idoru = new Idoru();
         assertEquals("나 아이돌 그만둘래!", idoru.yameru());
     }
 }
