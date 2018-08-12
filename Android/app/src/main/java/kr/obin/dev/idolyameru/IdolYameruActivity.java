1ea25d246f1ab05935612de2f0e76fa239e7e41d
JH Kwon <dev@obin.kr>
2018-08-13 04:36:43 +0900

    ICS 이하는 아이돌을 그만둘 수 없습니다

---
+++ Android/app/src/main/java/kr/obin/dev/idolyameru/IdolYameruActivity.java
@@ -0,0 +1,15 @@
+package kr.obin.dev.idolyameru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IdolYameruActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idol_yameru);
+    }
+}
